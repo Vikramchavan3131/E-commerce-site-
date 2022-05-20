dfbbe60123be0e8b88e6bd0895c4aab3f44540ff
Vikramchavan3131 <vikram.chavan@neosoftmail.com>
2022-05-21 00:39:18 +0530

    Initial commit

---
+++ store/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from .models import *
+from django.contrib.auth import authenticate
+
+
+def store(request):
+	products= Product.objects.all()
+	context = {"products" :products}
+	return render(request, 'store/store.html', context)
+
+def cart(request):
+	
+	if request.user.is_authenticated:
+			customer = request.user.Customer 
+			order, created = Order.objects.get_or_create(Customer=customer, complete= False)
+			items= order.orderitem_set.all()
+	else:
+		items=[]
+		order={'get_cart_total': 0, 'get_cart_items':0}
+	
+	context = {'items': items, 'order': order}
+	return render(request, 'store/cart.html', context)
+
+def checkout(request):
+	if request.user.is_authenticated:
+			customer = request.user.Customer 
+			order, created = Order.objects.get_or_create(Customer = Customer, complete= False)
+			items= order.orderitem_set.all()
+	else:
+		items=[]
+		order={'get_cart_total': 0, 'get_cart_items':0}
+	context = {'items': items, 'order': order}
+	return render(request, 'store/checkout.html', context)
